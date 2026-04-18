5225884ef340a913adea88c74fc48a3a6424921b
X4X TEAM ! <chamuzxmd@gmail.com>
2026-04-18 15:48:26 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  BOT_NAME: 'SASA-CODER',
+  SESSION_PREFIX: 'SASA',
+  AUTHOR: 'Silent Wolf',
+  DEPLOY_HOST: 'wolfxpair.xwolf.space',
+  PANEL_HOST: 'wolfxpair.xwolf.space',
+  GITHUB_URL: 'https://github.com/chamuzxmd-v2',
+  PORT: process.env.PORT || 3000,
+};
+
